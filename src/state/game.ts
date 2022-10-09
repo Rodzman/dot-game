3d3ea325a76e8fbc75f84a7b84b09a0e8096ab66
Rodrigo Pascoalino <designrsp@gmail.com>
2022-10-09 09:05:40 -0300

    DG3 - Persist game state

--- src/state/game.ts
+++ src/state/game.ts
@@ -1,18 +1,28 @@
 import { boardCols } from "const";
 import { atom } from "recoil";
 import { Board, Player } from "types";
+import { localStorageEffect } from "./utils";
 
 export const boardState = atom<Board>({
   key: "boardState",
   default: Array(boardCols).fill([]),
+  effects: [
+    localStorageEffect('boardState'),
+  ]
 });
 
 export const playerState = atom<Player>({
   key: "playerState",
   default: 1,
+  effects: [
+    localStorageEffect('playerState'),
+  ]
 });
 
 export const gameOverState = atom<boolean>({
   key: "gameOverState",
   default: false,
+  effects: [
+    localStorageEffect('gameOverState'),
+  ]
 });
